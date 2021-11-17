df63d2b74dade65d94497e3c074e043d3ca7a802
Joshua Flores <joshuaflores@joshuas-mbp.myfiosgateway.com>
2021-11-17 11:07:42 -0500

    updated design process

--- src/components/facts.js
+++ src/components/facts.js
@@ -38,22 +38,33 @@ const Facts = () => {
         <StyledFactNumber>
           <span className="number">1. Listen</span>
           <span className="fact">
-            Good designers don't talk, they ask questions and listen. During this phase I want to find out everything I
-            can to help better inform my design decisions.
+            {
+              "Good designers don't talk, they ask questions and listen. During this phase I'm like a detective - speaking to everyone from users to stakeholders to find out everything necessary for a well-informed design."
+            }
           </span>
         </StyledFactNumber>
         <StyledFactNumber>
           <span className="number">2. Design</span>
           <span className="fact">
-            This is where I translate what I've learned into mockups of an actual product. The designs at this point
-            aren't meant to be perfect - they're meant to be conversation starters.
+            {
+              "The design phase is where I translate what I've learned into flows and eventually mockups of the actual product. The designs at this point aren't meant to be perfect - they're meant to be conversation starters."
+            }
           </span>
         </StyledFactNumber>
         <StyledFactNumber>
           <span className="number">3. Build</span>
           <span className="fact">
-            This is where I put it all together, delivering work in small chunks and soliciting frequent feedback. This
-            process continues until we're ready to pull the trigger and ship.
+            {
+              'This is where I put it all together - either creating the product myself or writing user stories for engineering to deliver. I like to focus on delivering work in small chunks and soliciting feedback until the stakeholder is ready to go live. '
+            }
+          </span>
+        </StyledFactNumber>
+        <StyledFactNumber>
+          <span className="number">...and repeat</span>
+          <span className="fact">
+            {
+              "Development doesn't stop once a product goes out. After a product is launched the work begins of monitoring user behavior and soliciting feedback. These learnings are then incorporated into the beginning of another iterative design cycle. "
+            }
           </span>
         </StyledFactNumber>
       </StyledLogoSection>
